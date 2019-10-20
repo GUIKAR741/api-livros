290266d0aea3c700465c09acd8dbc6fc1a035732
Aureliano <aurelianosza@gmail.com>
2019-10-20 00:38:49 -0300

    Readme atualizado

--- api_livros/app/controllers/bookController.js
+++ api_livros/app/controllers/bookController.js
@@ -14,7 +14,15 @@ var index = function(req, res){
 
 var show = function(req, res){
    book.findById(req.params.id, '_id titulo ano_publicacao autor capitulos._id capitulos.nome').exec(function(err, book){
-        res.json(book);
+       if(err){
+           res.status(400);
+           res.end(err);
+       }else if (book == null) {
+           res.status(404);
+           res.end();
+       }else{
+           res.json(book);
+        }
    });
 }
 
@@ -37,26 +45,35 @@ var create = function(req, res){
 }
 
 var update = function(req, res){
-    book.findByIdAndUpdate(req.params.id, {titulo: req.body.titulo, ano_publicacao: req.body.ano_publicacao}, function(err){
-        if(err){    
+    book.findByIdAndUpdate(req.params.id, {titulo: req.body.titulo, ano_publicacao: req.body.ano_publicacao}, function(err, resp){
+
+
+        if(resp == null) {
+            res.status(404);
+            res.end();
+        }else if(err){    
             res.status(400);
             res.json(err);
+        }else{
+            res.status(200);
+            res.end();
         }
-        res.status(200);
-        res.end();
     })
 }
 
 var destroy = function(req, res){
-    book.findByIdAndDelete(req.params.id, function(err){
-        if(err){
+    book.findByIdAndDelete(req.params.id, function(err, resp){
+
+        if(resp == null) {
+            res.status(404);
+            res.end();
+        }else if(err){
             res.status(400);
-            res.json({
-                error: err.message
-            });
+            res.end();
+        }else{
+            res.status(200);
+            res.end();
         }
-        res.status(200);
-        res.end();
     });
 }
 
--- api_livros/app/controllers/chapterController.js
+++ api_livros/app/controllers/chapterController.js
@@ -2,60 +2,109 @@ var livro = require('../models/book');
 
 var index = function(req, res){
     livro.findById(req.params.id_livro, 'capitulos._id capitulos.nome',function(err, results){
-        if(err){
-            res.status(400);
-            res.json({
-                error:err.message
-            });
+
+        if(results == null){
+            res.status(404);
             res.end();
+        }else if(err){
+            res.status(400);
+            res.end();
+        }else{
+            res.json(results.capitulos);
         }
-        res.json(results.capitulos);
     });
 }
 
 var show = function(req, res){
     livro.findById(req.params.id_livro, function(err, results){
-        var chapter = results.capitulos.id(req.params.id);
-        res.json(chapter);
+
+        if(results == null) {
+            res.status(404);
+            res.end();
+        }else {
+
+            var chapter = results.capitulos.id(req.params.id);
+
+            if(chapter == null) {
+                res.status(404);
+                res.end();
+            }else {
+                res.json(chapter);
+            }
+
+        }
     });
 }
 
 
 var create = function(req, res){
     livro.findById(req.params.id_livro, function(err, results){
-        if(err){
+        if(results == null ){
+            res.status(404);
+            res.end();
+        }else if(err){
             res.status(400);
             res.end();
+        }else{  
+            results.capitulos.push({nome:req.body.nome, conteudo: req.body.conteudo});
+            results.save();
+            res.status(201);
+            res.end();
         }
-        results.capitulos.push({nome:req.body.nome, conteudo: req.body.conteudo});
-        results.save();
-        res.status(201);
-        res.end();
     });
 }
 
 var update = function(req, res){
     livro.findById(req.params.id_livro, function(err, results){
-        var chapter = results.capitulos.id(req.params.id);
-        chapter.nome = req.body.nome;
-        chapter.conteudo = req.body.conteudo;
+        if(results == null){
+            res.status(404);
+            res.end();
+        }else{
 
-        results.save();
+                   
+            var chapter = results.capitulos.id(req.params.id);
 
-        res.status(200);
-        res.end();
+            if(chapter == null) {
+                res.status(404);
+                res.end();
+            }else{
+                chapter.nome = req.body.nome;
+                chapter.conteudo = req.body.conteudo;
+                
+                results.save();
+                
+                res.status(200);
+                res.end();
+            }
+    
+        }
     });
 }
 
 var destroy = function(req, res){
     livro.findById(req.params.id_livro, function(err, results){
-        var chapter = results.capitulos.id(req.params.id);
-        chapter.remove();
 
-        results.save();
+        if(results == null){
+            res.status(404);
+            res.end();
+        }else{
+              
+            var chapter = results.capitulos.id(req.params.id);
 
-        res.status(200);
-        res.end();
+            if(chapter == null) {
+                res.status(404);
+                res.end();
+            }else{
+                chapter.remove();
+            
+            
+                results.save();
+                
+                res.status(200);
+                res.end();
+            }
+           
+        }
     });
 }
 
